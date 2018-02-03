33a86b357976946617f08076e9d00ea9ceffea25
TilakDhulipalla <s528817@mail.nwmissouri.edu>
2018-02-02 23:05:15 -0600

    working on push notifications

---
+++ SampleApplication/app/src/main/java/com/example/s528817/sampleapplication/MyFirebaseInstanceIDService.java
@@ -0,0 +1,10 @@
+package com.example.s528817.sampleapplication;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+
+/**
+ * Created by S528817 on 2/1/2018.
+ */
+
+public class MyFirebaseInstanceIDService extends FirebaseMessagingService{
+}
---
+++ SampleApplication/app/src/main/java/com/example/s528817/sampleapplication/MyFirebaseMessagingService.java
@@ -0,0 +1,8 @@
+package com.example.s528817.sampleapplication;
+
+/**
+ * Created by S528817 on 2/1/2018.
+ */
+
+public class MyFirebaseMessagingService {
+}
